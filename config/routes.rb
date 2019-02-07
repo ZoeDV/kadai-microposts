8062c83e89757404e450a55a72295f0c5fb49eca
zoe.study <zoestuudy@gmail.com>
2019-02-07 09:57:13 +0000

    likes

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,9 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      
+      get :likes
+      #get :favorite_users
     end
     #collection do
     #  get :search
@@ -38,4 +41,5 @@ Rails.application.routes.draw do
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
